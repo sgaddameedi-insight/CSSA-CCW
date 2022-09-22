d002a0dc4466674eef3a99af98ef70eb45f42a99
jeremiahbooth <jboothwebdev@gmail.com>
2022-09-22 11:34:41 -0600

    Updated the validations for the forms.

--- UI/libs/shared/ui/src/lib/components/form-stepper/form-steps/FormStepOne.vue
+++ UI/libs/shared/ui/src/lib/components/form-stepper/form-steps/FormStepOne.vue
@@ -1,9 +1,16 @@
 <template>
   <div>
-    <v-form>
+    <v-form
+      ref="form"
+      v-model="valid"
+    >
       <v-subheader class="subHeader font-weight-bold"> Personal Information</v-subheader>
       <v-row class="ml-5">
-        <v-col cols="6" md="5" sm="3">
+        <v-col
+          cols="6"
+          md="5"
+          sm="3"
+        >
           <v-text-field
             v-model="personalInfo.lastName"
             label="Last name"
@@ -13,7 +20,11 @@
           </v-text-field>
         </v-col>
 
-        <v-col cols="6" md="5" sm="3">
+        <v-col
+          cols="6"
+          md="5"
+          sm="3"
+        >
           <v-text-field
             v-model="personalInfo.firstName"
             label="First name"
@@ -23,7 +34,11 @@
           </v-text-field>
         </v-col>
 
-        <v-col cols="6" md="5" sm="3">
+        <v-col
+          cols="6"
+          md="5"
+          sm="3"
+        >
           <v-text-field
             v-model="personalInfo.middleName"
             label="Middle name"
@@ -35,12 +50,26 @@
           </v-text-field>
         </v-col>
 
-        <v-col cols="6" md="5" sm="3">
-          <v-text-field v-model="personalInfo.suffix" label="suffix"> </v-text-field>
+        <v-col
+          cols="6"
+          md="5"
+          sm="3"
+        >
+          <v-text-field
+            v-model="personalInfo.suffix"
+            label="suffix"
+          />
         </v-col>
 
-        <v-col cols="6" md="5">
-          <v-checkbox class="ml-3" v-model="personalInfo.noMiddleName" :label="'No middle name'"> </v-checkbox>
+        <v-col
+          cols="6"
+          md="5"
+        >
+          <v-checkbox
+            class="ml-3"
+            v-model="personalInfo.noMiddleName"
+            :label="'No middle name'"
+          />
         </v-col>
       </v-row>
       <v-divider class="my-3" />
@@ -48,23 +77,60 @@
       <v-subheader class="subHeader font-weight-bold"> Social Security Information </v-subheader>
 
       <v-row class="ml-5">
-        <v-col cols="7" md="5" m="3">
-          <v-text-field v-model="personalInfo.ssn" label="SSN" :rules="[v => !!v || 'SSN is required']" required>
-          </v-text-field>
+        <v-col
+          cols="7"
+          md="5"
+          m="3"
+        >
+          <!-- TODO: Add further validation to this once we decide of SSN formatting -->
+          <v-text-field
+            v-model="personalInfo.ssn"
+            label="SSN"
+            :rules="[v => !!v || 'SSN is required']"
+            required
+          />
         </v-col>
 
-        <v-col cols="7" md="5" sm="3">
-          <v-text-field v-model="ssnConfirm" label="Confirm SSN" :rules="ssnConfirmError" required> </v-text-field>
+        <v-col
+          cols="7"
+          md="5"
+          sm="3"
+        >
+          <v-text-field
+            v-model="ssnConfirm"
+            label="Confirm SSN"
+            :rules="[v => !!v && ssnConfirmError]"
+            required
+          />
         </v-col>
       </v-row>
 
       <v-divider class="my-3" />
       <v-row class="ml-1">
         <v-subheader class="subHeader font-weight-bold"> Marital status </v-subheader>
-        <v-col cols="16" md="5" sm="3">
-          <v-radio-group class="ml-3 mt-0" row v-model="personalInfo.maritalStatus">
-            <v-radio class="pb-1" label="Married" value="married" />
-            <v-radio class="pb-1" label="Single" value="single" />
+        <v-col
+          cols="16"
+          md="5"
+          sm="3"
+        >
+          <v-radio-group
+            class="ml-3 mt-0"
+            row
+            :hint="'You must select a value'"
+            :persistent-hint="true"
+            :rules="[v => !!v || 'You must select a status']"
+            v-model="personalInfo.maritalStatus"
+          >
+            <v-radio
+              class="pb-1"
+              label="Married"
+              value="married"
+            />
+            <v-radio
+              class="pb-1"
+              label="Single"
+              value="single"
+            />
           </v-radio-group>
         </v-col>
       </v-row>
@@ -77,7 +143,13 @@
       <AliasDialog :save-alias="getAliasFromDialog" />
     </div>
     <div class="form-btn-container">
-      <v-btn color="secondary mr-2" @click="handleSubmit"> Continue </v-btn>
+      <v-btn
+        color="secondary mr-2"
+        @click="handleSubmit"
+        :disabled="!valid"
+      >
+        Continue
+      </v-btn>
       <!-- TODO: this needs to save to save the current state to local storage or call the api to save in
       the db
        -->
@@ -85,22 +157,19 @@
       <!-- TODO: Make this return to the home page with out saving the form at all -->
       <v-btn color="error mr-2"> Cancel</v-btn>
     </div>
-    <FormErrorAlert v-if="errors.length > 0" :errors="errors" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, PropType } from 'vue';
+import { mapActions } from 'vuex';
 import AliasDialog from '../../dialogs/AliasDialog';
 import AliasTable from '../../tables/AliasTable';
-import FormErrorAlert from '@shared-ui/components/alerts/FormErrorAlert.vue';
-import { addPersonalInfoAction, addAliasesAction } from '@shared-ui/helpers/storeHelpers/storeActions';
 import { Alias, PersonalInfo } from '@shared-ui/types/defualtTypes';
-import { validateFormStepOne } from '@shared-ui/validators/form-validators/formStepOneValidators';
 
 export default defineComponent({
   name: 'FormStepOne',
-  components: { AliasTable, AliasDialog, FormErrorAlert },
+  components: { AliasTable, AliasDialog },
   props: {
     handleNextSection: {
       type: Function as PropType<() => void>,
@@ -112,7 +181,7 @@ export default defineComponent({
       personalInfo: {} as PersonalInfo,
       ssnConfirm: '' as string,
       aliases: [] as Array<Alias>,
-      errors: [] as Array<string>,
+      valid: false,
     };
   },
   computed: {
@@ -127,18 +196,16 @@ export default defineComponent({
       return errors;
     },
   },
+
   methods: {
+    ...mapActions({
+      addAlias: 'addAlias',
+      addPersonalInfo: 'addPersonalInfo',
+    }),
     handleSubmit() {
-      const formData = {
-        personalInfo: this.personalInfo,
-        ssnConfirm: this.ssnConfirm,
-      };
-      this.errors = validateFormStepOne(formData);
-      if (this.errors.length <= 0) {
-        addPersonalInfoAction(this.personalInfo);
-        addAliasesAction(this.aliases);
-        this.handleNextSection();
-      }
+      this.addPersonalInfo(this.personalInfo);
+      this.addAlias(this.aliases);
+      this.handleNextSection();
     },
 
     getAliasFromDialog(alias) {
@@ -148,7 +215,7 @@ export default defineComponent({
 });
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .subHeader {
   font-size: 1.5rem;
 }
--- UI/libs/shared/ui/src/lib/components/form-stepper/form-steps/FormStepTwo.vue
+++ UI/libs/shared/ui/src/lib/components/form-stepper/form-steps/FormStepTwo.vue
@@ -1,9 +1,16 @@
 <template>
   <div>
-    <v-form>
+    <v-form
+      ref="form"
+      v-model="valid"
+    >
       <v-subheader class="subHeader font-weight-bold"> Id Information </v-subheader>
       <v-row>
-        <v-col cols="6" md="5" sm="3">
+        <v-col
+          cols="6"
+          md="5"
+          sm="3"
+        >
           <v-text-field
             v-model="id.idNumber"
             label="Id number"
@@ -12,7 +19,11 @@
           />
         </v-col>
 
-        <v-col cols="6" md="5" sm="3">
+        <v-col
+          cols="6"
+          md="5"
+          sm="3"
+        >
           <v-text-field
             v-model="id.issuingState"
             label=" Issuing State"
@@ -21,39 +32,122 @@
           />
         </v-col>
       </v-row>
+
       <v-divider />
       <v-subheader class="subHeader font-weight-bold"> Date of birth </v-subheader>
 
       <v-row>
-        <v-col cols="6" md="5" sm="3">
-          <v-date-picker v-model="DOBInfo.DOB" label="Date of birth" />
-          <!-- TODO: need a error block here if the data picker is invalid -->
+        <v-col
+          cols="6"
+          md="5"
+          sm="3"
+        >
+          <v-date-picker
+            v-model="DOBInfo.DOB"
+            label="Date of birth"
+          />
+          <v-alert
+            dense
+            outlined
+            type="error"
+            v-if="!DOBInfo.DOB"
+          >
+            Date of birth cannot be blank!
+          </v-alert>
         </v-col>
-        <v-col cols="6" md="5" sm="3">
-          <v-text-field v-model="DOBInfo.birthCity" label="Birth city" required />
-          <v-text-field v-model="DOBInfo.birthState" label="Birth state" />
-          <v-text-field v-model="DOBInfo.birthCountry" label="Birth country" required />
+
+        <v-col
+          cols="6"
+          md="5"
+          sm="3"
+        >
+          <v-text-field
+            v-model="DOBInfo.birthCity"
+            label="Birth city"
+            :rules="[v => !!v || 'Birth city cannot be blank']"
+            required
+          />
+          <v-text-field
+            v-model="DOBInfo.birthState"
+            label="Birth state"
+            :rules="[v => !!v || 'Birth state cannot be blank']"
+          />
+          <v-text-field
+            v-model="DOBInfo.birthCountry"
+            label="Birth country"
+            required
+            :rules="[v => !!v || 'Birth country cannot be blank']"
+          />
         </v-col>
       </v-row>
 
       <v-divider />
       <v-subheader class="subHeader font-weight-bold"> Citizenship Information </v-subheader>
-      <v-row>
-        <v-col cols="6" md="5" sm="3">
-          <v-radio-group class="ml-5" v-model="citizenshipInfo.citizen" label="Citizen">
-            <v-radio label="Yes" :value="true" />
 
-            <v-radio label="No" :value="false" />
+      <v-row>
+        <v-col
+          cols="6"
+          md="5"
+          sm="3"
+        >
+          <v-radio-group
+            class="ml-5"
+            row
+            v-model="citizenshipInfo.citizen"
+            label="Citizen"
+          >
+            <v-radio
+              label="Yes"
+              :value="true"
+            />
+
+            <v-radio
+              label="No"
+              :value="false"
+            />
           </v-radio-group>
+
+          <v-alert
+            dense
+            outlined
+            type="error"
+            v-if="!citizenshipInfo.citizen"
+          >
+            Must select Yes or No!
+          </v-alert>
         </v-col>
-        <v-col cols="6" md="5" sm="3">
-          <v-select v-model="citizenshipInfo.militaryStatus" :items="items" label="Military Status"> </v-select>
+
+        <v-col
+          cols="6"
+          md="5"
+          sm="3"
+        >
+          <v-select
+            v-model="citizenshipInfo.militaryStatus"
+            :items="items"
+            label="Military Status"
+          />
+
+          <v-alert
+            dense
+            outlined
+            type="error"
+            v-if="!citizenshipInfo.militaryStatus"
+          >
+            Must select a status!
+          </v-alert>
         </v-col>
       </v-row>
     </v-form>
     <v-divider />
     <div class="form-btn-container">
-      <v-btn color="secondary mr-2" @click="handleSubmit"> Continue </v-btn>
+      <v-btn
+        :disabled="!valid"
+        color="secondary mr-2"
+        @click="handleSubmit"
+      >
+        Continue
+      </v-btn>
       <!-- TODO: this needs to save to save the current state to local storage or call the api to save in
       the db
        -->
@@ -61,20 +155,16 @@
       <!-- TODO: Make this return to the home page with out saving the form at all -->
       <v-btn color="error mr-2"> Cancel</v-btn>
     </div>
-    <FormErrorAlert v-if="errors.length > 0" :errors="errors" />
   </div>
 </template>
 
 <script lang="ts">
+import { mapActions } from 'vuex';
 import { defineComponent, PropType } from 'vue';
 import { Citizenship, DOB, Id } from '@shared-ui/types/defualtTypes';
-import { validateForm } from '@shared-ui/validators/form-validators/formStepTwoValidators';
-import FormErrorAlert from '@shared-ui/components/alerts/FormErrorAlert.vue';
-import { addIdAction, addDOBAction, addCitizenshipAction } from '@shared-ui/helpers/storeHelpers/storeActions';
 
 export default defineComponent({
   name: 'FormStepTwo',
-  components: { FormErrorAlert },
   props: {
     handleNextSection: {
       type: Function as PropType<() => void>,
@@ -87,23 +177,20 @@ export default defineComponent({
       DOBInfo: {} as DOB,
       citizenshipInfo: {} as Citizenship,
       items: ['Active', 'Reserve', 'Discharged', 'Retired', 'N/A'],
-      errors: [] as Array<string>,
+      valid: false,
     };
   },
   methods: {
+    ...mapActions({
+      addId: 'addId',
+      addDOB: 'addDOB',
+      addCitizenship: 'addCitizenshipInfo',
+    }),
     handleSubmit() {
-      const formData = {
-        id: this.id,
-        dob: this.DOBInfo,
-        citizenship: this.citizenshipInfo,
-      };
-      this.errors = validateForm(formData);
-      if (this.errors.length <= 0) {
-        addIdAction(this.id);
-        addDOBAction(this.DOBInfo);
-        addCitizenshipAction(this.citizenshipInfo);
-        this.handleNextSection();
-      }
+      this.addId(this.id);
+      this.addDOB(this.DOBInfo);
+      this.addCitizenship(this.citizenshipInfo);
+      this.handleNextSection();
     },
   },
 });
