46868b83ce46a08c7b8dbf2b37320adb4d374615
jeremiahbooth <jboothwebdev@gmail.com>
2022-09-27 06:20:49 -0700

    unsused files deleted

--- UI/libs/shared/ui/src/lib/helpers/storeHelpers/citizenActions.ts
+++
@@ -1,6 +0,0 @@
-import { store } from '@shared-ui/store';
-import { Citizenship } from '@shared-ui/types/defualtTypes';
-
-export async function addCitizenshipAction(citizenship: Citizenship) {
-  await store.dispatch('addCitizenshipInfo', citizenship);
-}
